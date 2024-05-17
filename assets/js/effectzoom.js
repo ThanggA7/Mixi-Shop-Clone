bcb39e7e9f6de4bfaa4b59ce6f657b2eb4a59fce
Thangga7 <beencules@gmail.com>
2024-05-17 11:27:54 +0700

    Update image size and position in effect zoom

--- assets/js/effectzoom.js
+++ assets/js/effectzoom.js
@@ -12,27 +12,31 @@ img.addEventListener("mouseleave", function () {
   mirro.style.display = "none";
 });
 
-check__mirro.addEventListener("mousemove", function (e) {
-  if (mirro.style.display === "block") {
-    mirro.style.top = `${e.clientY}px`;
-    mirro.style.left = `${e.clientX}px`;
-  }
-});
-
 img.addEventListener("mousemove", function (e) {
-  let w = this.offsetWidth;
-  let h = this.offsetHeight;
+  let rect = this.getBoundingClientRect();
+  let mwibX = e.clientX - rect.left;
+  let mwibY = e.clientY - rect.top;
 
-  let mwibX = e.pageX - this.offsetLeft;
-  let mwibY = e.pageY - this.offsetTop;
+  let w = rect.width;
+  let h = rect.height;
 
   let pMbX = (mwibX / w) * 100;
   let pMbY = (mwibY / h) * 100;
 
   mirro.style.backgroundPosition = `${pMbX}% ${pMbY}%`;
 
-  let scale = 2; // Tăng tỷ lệ phóng to
+  let scale = 2;
   mirro.style.backgroundSize = `${w * scale}px ${h * scale}px`;
 
+  mirro.style.top = `${e.clientY - mirro.offsetHeight / 2}px`;
+  mirro.style.left = `${e.clientX - mirro.offsetWidth / 2}px`;
+
   console.log(`${pMbX}% ${pMbY}%`);
+});
+
+check__mirro.addEventListener("mousemove", function (e) {
+  if (mirro.style.display === "block") {
+    mirro.style.top = `${e.clientY - mirro.offsetHeight / 2}px`;
+    mirro.style.left = `${e.clientX - mirro.offsetWidth / 2}px`;
+  }
 });
