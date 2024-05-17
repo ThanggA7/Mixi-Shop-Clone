1d4ae0f6e2080799179772a358131141534734d2
Thangga7 <beencules@gmail.com>
2024-05-17 08:28:59 +0700

    push

--- assets/js/effectzoom.js
+++ assets/js/effectzoom.js
@@ -2,21 +2,17 @@ var mirro = document.querySelector("#mirro");
 var img = document.querySelector("#image");
 var check__mirro = document.querySelector("#check__mirro");
 
-// Đặt hình ảnh nền cho kính lúp
 mirro.style.backgroundImage = `url(${img.src})`;
 
-// Hiển thị mirro khi di chuột vào img
 img.addEventListener("mouseenter", function () {
   mirro.style.display = "block";
 });
 
-// Ẩn mirro khi rời khỏi img
 img.addEventListener("mouseleave", function () {
   mirro.style.display = "none";
 });
 
 check__mirro.addEventListener("mousemove", function (e) {
-  // Chỉ di chuyển mirro nếu nó đang được hiển thị
   if (mirro.style.display === "block") {
     mirro.style.top = `${e.clientY}px`;
     mirro.style.left = `${e.clientX}px`;
